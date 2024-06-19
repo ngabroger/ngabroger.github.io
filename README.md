bff161734ec54ebf53221dbe34fc8a723255269d
Roger Simanjuntak <arbination13@gmail.com>
2024-06-19 15:48:35 +0700

    final update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 <h2>Portofolio Website</h2>
-<p>on my portofolio website there are many kind about of my self </p>
+<img src='/resource/images/images.jpeg'></img>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   theme: {
    
     extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-in forwards',
+        'fade-out': 'fadeOut 1s ease-in forwards',
+      },
+      transform: {
+        'fade-in': 'translateY(-100%) opacity(0) scale(0.5)',
+        'fade-out': 'translateY(0) opacity(1) scale(1)',
+      },
       colors: {
         primary:'#183D3D',
         secondary:'#040D12',
